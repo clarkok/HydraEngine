899e9a33c748bcd18fa86db9be63fa58e578f03c
Clarkok <mail@clarkok.com>
2017-04-15 19:56:28 +0800

    re-indent

--- Test/HydraCoreTest/JSObjectTest.cpp
+++ Test/HydraCoreTest/JSObjectTest.cpp
@@ -18,123 +18,123 @@ TEST_CASE("JSObjectTest", "[Runtime]")
     gc::Heap *heap = gc::Heap::GetInstance();
     gc::ThreadAllocator allocator(heap);
 
-Klass *emptyKlass = Klass::EmptyKlass(allocator);
-JSObject *uut = emptyKlass->NewObject<JSObject>(allocator);
-String *key = String::New(allocator, u"key");
-JSValue fetched;
-bool result = false;
+    Klass *emptyKlass = Klass::EmptyKlass(allocator);
+    JSObject *uut = emptyKlass->NewObject<JSObject>(allocator);
+    String *key = String::New(allocator, u"key");
+    JSValue fetched;
+    bool result = false;
 
-SECTION("Basic Test")
-{
-    JSValue value = JSValue::FromNumber(10);
-
-    fetched = uut->Get(key);
-    REQUIRE(!result);
-    REQUIRE(fetched == JSValue());
-
-    uut->Add(allocator, key, value);
-    auto originalUut = uut;
-    REQUIRE(JSObject::Latest(uut) == originalUut);
-
-    fetched = uut->Get(key);
-    REQUIRE(fetched == value);
-
-    uut->Delete(key);
-    fetched = uut->Get(key);
-    REQUIRE(fetched == JSValue());
-}
-
-SECTION("Undefined Test")
-{
-    JSValue undefined = JSValue::Undefined();
-
-    fetched = uut->Get(key);
-    REQUIRE(!result);
-    REQUIRE(fetched == JSValue());
-
-    uut->Add(allocator, key, undefined);
-    fetched = uut->Get(key);
-    REQUIRE(fetched == undefined);
-
-    uut->Delete(key);
-    fetched = uut->Get(key);
-    REQUIRE(fetched == JSValue());
-
-    result = uut->Set(key, undefined);
-    REQUIRE(result);
-
-    fetched = uut->Get(key);
-    REQUIRE(fetched == undefined);
-}
-
-SECTION("Enlarge Test")
-{
-    String *ALPHABET = String::New(allocator, u"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    auto originalUut = uut;
-
-    for (size_t i = 0; i < ALPHABET->length(); ++i)
+    SECTION("Basic Test")
     {
-        auto key = String::Slice(allocator, ALPHABET, i, 1);
-        auto value = JSValue::FromSmallInt(i);
+        JSValue value = JSValue::FromNumber(10);
 
-        JSObject::Latest(uut)->Add(allocator, key, value);
+        fetched = uut->Get(key);
+        REQUIRE(!result);
+        REQUIRE(fetched == JSValue());
+
+        uut->Add(allocator, key, value);
+        auto originalUut = uut;
+        REQUIRE(JSObject::Latest(uut) == originalUut);
+
+        fetched = uut->Get(key);
+        REQUIRE(fetched == value);
+
+        uut->Delete(key);
+        fetched = uut->Get(key);
+        REQUIRE(fetched == JSValue());
     }
 
-    REQUIRE(uut != originalUut);
-
-    for (size_t i = 0; i < ALPHABET->length(); ++i)
+    SECTION("Undefined Test")
     {
-        auto key = String::Slice(allocator, ALPHABET, i, 1);
+        JSValue undefined = JSValue::Undefined();
 
-        REQUIRE(uut->Get(key) == JSValue::FromSmallInt(i));
-    }
-}
+        fetched = uut->Get(key);
+        REQUIRE(!result);
+        REQUIRE(fetched == JSValue());
 
-SECTION("Index and Offset Test")
-{
-    String *ALPHABET = String::New(allocator, u"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        uut->Add(allocator, key, undefined);
+        fetched = uut->Get(key);
+        REQUIRE(fetched == undefined);
 
-    for (size_t i = 0; i < ALPHABET->length(); ++i)
-    {
-        auto key = String::Slice(allocator, ALPHABET, i, 1);
-        auto value = JSValue::FromSmallInt(i);
+        uut->Delete(key);
+        fetched = uut->Get(key);
+        REQUIRE(fetched == JSValue());
 
-        JSObject::Latest(uut)->Add(allocator, key, value);
-    }
-
-    auto visitUutByIndex = [&](size_t index) -> JSValue
-    {
-        auto latest = JSObject::Latest(uut);
-        return *reinterpret_cast<JSValue*>(
-            reinterpret_cast<uintptr_t>(latest) + sizeof(JSObject) + sizeof(JSValue) * index);
-    };
-
-    auto visitUutByOffset = [&](size_t offset) -> JSValue
-    {
-        auto latest = JSObject::Latest(uut);
-        return *reinterpret_cast<JSValue*>(
-            reinterpret_cast<uintptr_t>(latest) + offset);
-    };
-
-    for (size_t i = 0; i < ALPHABET->length(); ++i)
-    {
-        auto key = String::Slice(allocator, ALPHABET, i, 1);
-        auto expected = JSValue::FromSmallInt(i);
-
-        auto fetched = uut->Get(key);
-        REQUIRE(fetched == expected);
-
-        size_t index = 0;
-        result = uut->Index(key, index);
+        result = uut->Set(key, undefined);
         REQUIRE(result);
-        REQUIRE(visitUutByIndex(index) == expected);
 
-        size_t offset = 0;
-        result = uut->Offset(key, offset);
-        REQUIRE(result);
-        REQUIRE(visitUutByOffset(offset) == expected);
+        fetched = uut->Get(key);
+        REQUIRE(fetched == undefined);
     }
-}
+
+    SECTION("Enlarge Test")
+    {
+        String *ALPHABET = String::New(allocator, u"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        auto originalUut = uut;
+
+        for (size_t i = 0; i < ALPHABET->length(); ++i)
+        {
+            auto key = String::Slice(allocator, ALPHABET, i, 1);
+            auto value = JSValue::FromSmallInt(i);
+
+            JSObject::Latest(uut)->Add(allocator, key, value);
+        }
+
+        REQUIRE(uut != originalUut);
+
+        for (size_t i = 0; i < ALPHABET->length(); ++i)
+        {
+            auto key = String::Slice(allocator, ALPHABET, i, 1);
+
+            REQUIRE(uut->Get(key) == JSValue::FromSmallInt(i));
+        }
+    }
+
+    SECTION("Index and Offset Test")
+    {
+        String *ALPHABET = String::New(allocator, u"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+        for (size_t i = 0; i < ALPHABET->length(); ++i)
+        {
+            auto key = String::Slice(allocator, ALPHABET, i, 1);
+            auto value = JSValue::FromSmallInt(i);
+
+            JSObject::Latest(uut)->Add(allocator, key, value);
+        }
+
+        auto visitUutByIndex = [&](size_t index) -> JSValue
+        {
+            auto latest = JSObject::Latest(uut);
+            return *reinterpret_cast<JSValue*>(
+                reinterpret_cast<uintptr_t>(latest) + sizeof(JSObject) + sizeof(JSValue) * index);
+        };
+
+        auto visitUutByOffset = [&](size_t offset) -> JSValue
+        {
+            auto latest = JSObject::Latest(uut);
+            return *reinterpret_cast<JSValue*>(
+                reinterpret_cast<uintptr_t>(latest) + offset);
+        };
+
+        for (size_t i = 0; i < ALPHABET->length(); ++i)
+        {
+            auto key = String::Slice(allocator, ALPHABET, i, 1);
+            auto expected = JSValue::FromSmallInt(i);
+
+            auto fetched = uut->Get(key);
+            REQUIRE(fetched == expected);
+
+            size_t index = 0;
+            result = uut->Index(key, index);
+            REQUIRE(result);
+            REQUIRE(visitUutByIndex(index) == expected);
+
+            size_t offset = 0;
+            result = uut->Offset(key, offset);
+            REQUIRE(result);
+            REQUIRE(visitUutByOffset(offset) == expected);
+        }
+    }
 }
 
 }
